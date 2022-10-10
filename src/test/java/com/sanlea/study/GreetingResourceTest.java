b0d5ff523249b1d379d217aee4f98fe36c7bbfe0
Kut Zhang <kut@sanlea.cn>
2022-10-10 23:00:38 +0800

    update for test.

--- src/test/java/com/sanlea/study/GreetingResourceTest.java
+++ src/test/java/com/sanlea/study/GreetingResourceTest.java
@@ -1,13 +1,22 @@
 package com.sanlea.study;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 class GreetingResourceTest {
+
+    private GreetingResource greetingResource;
+
+    @BeforeEach
+    void setup() {
+        this.greetingResource = new GreetingResource();
+    }
 
     @Test
     void testHelloEndpoint() {
@@ -16,6 +25,18 @@ class GreetingResourceTest {
           .then()
              .statusCode(200)
              .body(is("Hello RESTEasy"));
+    }
+
+    @Test
+    void testHello() {
+        var message = this.greetingResource.hello();
+        assertEquals("Hello RESTEasy", message);
+    }
+
+    @Test
+    void testShowMeTheMoney() {
+        var message = this.greetingResource.showMeTheMoney();
+        assertEquals("Show me the money", message);
     }
 
 }
