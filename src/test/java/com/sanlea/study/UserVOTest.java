84b5e533caa599209e2268702175ef9e79ca3428
Kut Zhang <kut@sanlea.cn>
2022-10-10 23:16:55 +0800

    for test

---
+++ src/test/java/com/sanlea/study/UserVOTest.java
@@ -0,0 +1,21 @@
+package com.sanlea.study;
+
+import com.sanlea.study.vo.UserVO;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@QuarkusTest
+public class UserVOTest {
+    @Test
+    void all() {
+        var user = new UserVO();
+        user.setId(UUID.randomUUID().toString());
+        user.setName("kut");
+        user.setAge(42);
+        assertNotNull(user.toString());
+    }
+}
