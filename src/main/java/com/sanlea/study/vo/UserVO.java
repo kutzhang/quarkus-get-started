a1214490a3cdc702bb9b5a268dcca12846c1cf3c
Kut Zhang <kut@sanlea.cn>
2022-10-10 23:19:55 +0800

    for test

--- src/main/java/com/sanlea/study/vo/UserVO.java
+++ src/main/java/com/sanlea/study/vo/UserVO.java
@@ -3,12 +3,13 @@ package com.sanlea.study.vo;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.*;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @RegisterForReflection
 @ToString
+@Setter
+@Getter
 public class UserVO {
     private String id;
     private String name;
